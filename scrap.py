ba9a12bf51210ae844c5831cb54658f936abe723
hugues wattez <wattez@MacBook-Air-de-hugues.local>
2024-05-23 12:09:29 +0200

    adding header to html page

--- scrap.py
+++ scrap.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import yaml
 from functools import cmp_to_key
 import codecs
+import glob
+import shutil
 
 path = 'index.html'
 data = []
@@ -12,9 +14,12 @@ with open(path) as f:
 
     for div in soup.select(".mix"):
         info_video = div.select('.infoVideo')[0]
+        id = div.select('a')[0].attrs['href'].split('/')[1]
+        img_to_move = glob.glob(f'img/videos/{id}/*.jpg')[0]
+        shutil.copyfile(img_to_move, f'img/videos/{id}.jpg')
 
         data.append({
-            'id': div.select('a')[0].attrs['href'].split('/')[1],
+            'id': id,
             'title': div.select('.videoTitle')[0].text,
             'description': info_video.attrs['data-description'],
             'youtube': info_video.attrs['data-youtube'],
